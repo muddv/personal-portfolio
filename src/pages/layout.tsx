96d681c01905f86ed1010d475caea173c1c027f1
misha <dedovmu@gmail.com>
2022-07-27 16:26:53 +0300

    Made sandwich navigation menu

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import sun from '../images/sun.svg'
+import menu from '../images/menu.svg'
 
 const Layout: NextPage = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -11,19 +12,22 @@ const Layout: NextPage = () => {
         darkMode ? document.body.classList.add("dark") : document.body.classList.remove("dark")
     }
 
+    const [navMenu, setNavMenu] = useState(false);
+    function showNavMenu() {
+        setNavMenu(!navMenu);
+    }
+
     return (
         <>
-            <header className="flex justify-end mr-11 gap-2">
-                <Link href="/">Home</Link> 
-                <Link href="/blog">Posts</Link>
+            <header className="flex ml-5 gap-2 w-21">
+                <div className="flex flex-none flex-col"><button onClick={showNavMenu} className='mt-10'><Image src={menu} alt="navigation menu"></Image></button>
+                    <div className={navMenu ? "mb-3 text-xl" : "hidden"}><Link href="/">Home</Link></div>
+                    <div className={navMenu ? "mb-3 text-xl" : "hidden"}><Link href="/blog">Posts</Link></div>
+                </div>
+                <button onClick={swichColorMode} className='flex justify-end ml-2 mt-10'>
+                    <Image src={sun} alt="switch light and dark mode" />
+                </button>
             </header>
-            <button onClick={swichColorMode}
-                className="ml-11">
-                <Image
-                    src={sun}
-                    alt="switch light and dark mode"
-                />
-            </button>
         </>
 
     )
